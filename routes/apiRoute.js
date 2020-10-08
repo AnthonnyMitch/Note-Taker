fb34b09b8b196a3766cc10b9928e150e930779a7
killermitch <anthonnymitch22@gmail.com>
2020-10-08 19:42:59 -0400

    apiRoute builded

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,7 +1,42 @@
+var db = require("../db/db.json");
+var store = require("../db/store");
+
+
+module.exports = function(app) {
+
+
+
+
 // GET ROUTE 
 
 
+app.get("/api/db", function(req, res) {
+    res.json(db);
+  });
+
+  app.get("/api/", function(req, res) {
+    res.json(store);
+  });
 // POST ROUTE 
+app.post("/api/db", function(req, res) {
+    
+    if (db.length) {
+      db.push(req.body);
+      res.json(true);
+    }
+    else {
+      store.push(req.body);
+      res.json(false);
+    }
+  });
 
 
 //DELETE 
+app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    db.length = 0;
+    store.length = 0;
+
+    res.json({ ok: true });
+  });
+};
